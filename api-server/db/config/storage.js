93e7fd807fb44ae752ac46247710453c72b9de1c
ims0rry <38806471+1M50RRY@users.noreply.github.com>
2019-06-17 12:23:13 +0300

    Check avatar file type

--- api-server/db/config/storage.js
+++ api-server/db/config/storage.js
@@ -1,12 +1,28 @@
 const multer = require('multer');
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/images')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+  destination: function (req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+  }
+})
+
+const accepted_extensions = ['jpg', 'png', 'gif'];
+exports.upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024,  // 5 MB upload limit
+    files: 1                    // 1 file
+  },
+  fileFilter: (req, file, cb) => {
+    // if the file extension is in our accepted list
+    if (accepted_extensions.some(ext => file.originalname.endsWith("." + ext))) {
+      return cb(null, true);
     }
-  })
-  
-exports.upload = multer({ storage: storage })
+
+    // otherwise, return error
+    return cb(new Error('Only ' + accepted_extensions.join(", ") + ' files are allowed!'));
+  }
+});
--- api-server/routes/user.js
+++ api-server/routes/user.js
@@ -2,9 +2,19 @@ var express = require('express');
 var router = express.Router();
 let users = require('../controllers/user');
 let multer = require('../db/config/storage')
+const fileType = require('file-type');
+
+validate_format = (req, res, next) => {
+    let mime = fileType(req.file.buffer);
+  
+    if(!mime || !accepted_extensions.includes(mime.ext))
+        return next(new Error('The uploaded file is not in ' + accepted_extensions.join(", ") + ' format!'));
+    
+    next();
+  }
 
 router.post('/login', users.login);
 router.get('/get', users.getUser)
-router.post('/add', multer.upload.single('avatar'), users.addUser);
+router.post('/add', multer.upload.single('avatar'), validate_format, users.addUser);
 
 module.exports = router;
